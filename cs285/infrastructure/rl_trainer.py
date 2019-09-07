881fb8318a14d439ce04d85b9da3070d6806e915
nagaban2 <nagaban2@berkeley.edu>
2019-09-06 17:40:31 -0700

    Update rl_trainer.py

--- cs285/infrastructure/rl_trainer.py
+++ cs285/infrastructure/rl_trainer.py
@@ -169,7 +169,7 @@ class RL_Trainer(object):
         if self.log_video:
             print('\nCollecting train rollouts to be used for saving videos...')
             ## TODO look in utils and implement sample_n_trajectories
-            train_video_paths = sample_n_trajectories(self.env, collect_policy_use, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+            train_video_paths = sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
 
         return paths, envsteps_this_batch, train_video_paths
 
