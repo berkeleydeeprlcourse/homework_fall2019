3e8afedb9f89dbde877219d460300ec664865ad1
mugoh <mugoh.ks@gmail.com>
2020-04-15 13:41:32 +0300

    collect rollouts of episode size

--- hw1/cs285/infrastructure/utils.py
+++ hw1/cs285/infrastructure/utils.py
@@ -5,6 +5,7 @@ import scipy
 ############################################
 ############################################
 
+
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
 
     # initialize env for the beginning of a new rollout
@@ -20,7 +21,8 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
         if render:
             if 'rgb_array' in render_mode:
                 if hasattr(env, 'sim'):
-                    image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
+                    image_obs.append(env.sim.render(
+                        camera_name='track', height=500, width=500)[::-1])
                 else:
                     image_obs.append(env.render(mode=render_mode))
             if 'human' in render_mode:
@@ -29,7 +31,8 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # use the most recent ob to decide what to do
         obs.append(ob)
-        ac = policy.get_action(ob) # TODO # HINT: query the policy's get_action function
+        # TODO # HINT: query the policy's get_action function
+        ac = policy.get_action(ob)
         ac = ac[0]
         acs.append(ac)
 
@@ -43,13 +46,15 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # TODO end the rollout if the rollout ended
         # HINT: rollout can end due to done, or due to max_path_length
-        rollout_done = done or steps >= max_path_length # TODO # HINT: this is either 0 or 1
+        # TODO # HINT: this is either 0 or 1
+        rollout_done = done or steps >= max_path_length
         terminals.append(rollout_done)
 
         if rollout_done:
             break
 
     return Path(obs, image_obs, acs, rewards, next_obs, terminals)
+
 
 def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
     """
@@ -63,9 +68,16 @@ def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, r
     paths = []
     while timesteps_this_batch < min_timesteps_per_batch:
 
-        TODO
+        path = sample_trajectory(env,
+                                 policy,
+                                 max_path_length,
+                                 render=render,
+                                 render_mode=render_mode)
+        paths.append(path)
+        timesteps_this_batch += get_pathlength(path)
 
     return paths, timesteps_this_batch
+
 
 def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, render_mode=('rgb_array')):
     """
@@ -83,6 +95,7 @@ def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, ren
 ############################################
 ############################################
 
+
 def Path(obs, image_obs, acs, rewards, next_obs, terminals):
     """
         Take info (separate arrays) from a single rollout
@@ -90,10 +103,10 @@ def Path(obs, image_obs, acs, rewards, next_obs, terminals):
     """
     if image_obs != []:
         image_obs = np.stack(image_obs, axis=0)
-    return {"observation" : np.array(obs, dtype=np.float32),
-            "image_obs" : np.array(image_obs, dtype=np.uint8),
-            "reward" : np.array(rewards, dtype=np.float32),
-            "action" : np.array(acs, dtype=np.float32),
+    return {"observation": np.array(obs, dtype=np.float32),
+            "image_obs": np.array(image_obs, dtype=np.uint8),
+            "reward": np.array(rewards, dtype=np.float32),
+            "action": np.array(acs, dtype=np.float32),
             "next_observation": np.array(next_obs, dtype=np.float32),
             "terminal": np.array(terminals, dtype=np.float32)}
 
@@ -110,12 +123,14 @@ def convert_listofrollouts(paths, concat_rew=True):
         rewards = np.concatenate([path["reward"] for path in paths])
     else:
         rewards = [path["reward"] for path in paths]
-    next_observations = np.concatenate([path["next_observation"] for path in paths])
+    next_observations = np.concatenate(
+        [path["next_observation"] for path in paths])
     terminals = np.concatenate([path["terminal"] for path in paths])
     return observations, actions, rewards, next_observations, terminals
 
 ############################################
 ############################################
+
 
 def get_pathlength(path):
     return len(path["reward"])
