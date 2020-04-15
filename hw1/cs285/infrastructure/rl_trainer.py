1cfb36a0b3ad633361debb6423a57a1a92d51e8f
mugoh <mugoh.ks@gmail.com>
2020-04-15 16:56:53 +0300

    relabel observations with expert policy

--- hw1/cs285/infrastructure/rl_trainer.py
+++ hw1/cs285/infrastructure/rl_trainer.py
@@ -205,6 +205,8 @@ class RL_Trainer(object):
         # TODO relabel collected obsevations (from our policy) with labels from an expert policy
         # HINT: query the policy (using the get_action function) with paths[i]["observation"]
         # and replace paths[i]["action"] with these expert labels
+        for path in paths:
+            path['action'] = expert_policy.get_action(path['observation'])
 
         return paths
 
